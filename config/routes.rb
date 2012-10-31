eb5c1ef14470ad740ca1035b3a14f60d323c53d4
Gijo Raj <gijoraj@vps.net>
2012-10-31 18:51:03 +0530

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Newblog::Application.routes.draw do
 
 
-  resources :articles do
-      resources :comments 
-  end
+  get "sessions/new"
+
+  resources :users
+
+ # resources :articles do
+ #     resources :comments
+ # end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +58,7 @@ Newblog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'application#index'
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121030100308) do
+ActiveRecord::Schema.define(:version => 20121031093648) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(:version => 20121030100308) do
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
