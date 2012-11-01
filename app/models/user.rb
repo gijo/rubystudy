52b89df94c062d24e3b8371d8e1b7d3daca8fad7
Gijo Raj <gijoraj@vps.net>
2012-11-01 17:06:10 +0530

    user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
-    #has_secure_password
+    has_secure_password
   validates_uniqueness_of :email
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Newblog::Application.routes.draw do
 
 
-  get "sessions/new"
+  #  get "sessions/new"
 
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   resources :users
+  resources :sessions
 
  # resources :articles do
  #     resources :comments
